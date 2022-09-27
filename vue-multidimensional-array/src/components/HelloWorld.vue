a64450d81e5e3f0907b519e4f0123ae62e159e4a
Omar Hernandez <omar.a.hernandez.d@gmail.com>
2022-09-27 16:55:39 -0500

    Adding  first Box dynamically

--- vue-multidimensional-array/src/components/HelloWorld.vue
+++ vue-multidimensional-array/src/components/HelloWorld.vue
@@ -1,38 +1,82 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+  import { ref , onMounted} from 'vue'
+  import type {Ref} from 'vue'
+  defineProps<{ msg: string }>()
+  
+  let ipBoxes:Ref<any> =ref([{}])
+  let containerHeadValues=ref({buttonMsj:'Start',ipAddress:undefined})
 
-defineProps<{ msg: string }>()
+  const createFirstRow=()=>{
+    if(containerHeadValues.value.buttonMsj==='Start' && containerHeadValues.value.ipAddress !==undefined ){
+      ipBoxes.value.push(
+        [
+          {id: 1, descripcion: 'Leo', ip: containerHeadValues.value.ipAddress, parent:''},
+        ]
+        )
+        containerHeadValues.value.buttonMsj='Restart'
+      }else{
+        ipBoxes.value=[]
+        containerHeadValues.value.ipAddress=undefined
+        containerHeadValues.value.buttonMsj='Start'
+    }
+  }
 
-const count = ref(0)
-</script>
+  onMounted(() => {
+    ipBoxes.value.shift()
+  })
+
+</script> 
 
 <template>
   <h1>{{ msg }}</h1>
+  <div class="container">
+    <div class="head">
+      <input v-model="containerHeadValues.ipAddress"  type="text">
+      <button @click="createFirstRow()">{{containerHeadValues.buttonMsj}}</button>
+    </div>
+    <div class="col" >
+      <div class="row" v-for="(ipBox,index) in ipBoxes" :key="index">
+       <span>L-{{ index }}=></span>
+       <span v-for="(box,boxindex) in ipBox" :key="boxindex" >
+        | {{ box.ip }} | 
+       </span>
+      </div>
+    </div>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
   </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
+
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
+  .container{
+    background-color: brown;
+    width: 400px;
+    height: 400px; 
+    padding: 20px;
+  }
+  .head{
+    margin:0 0 .5rem 0;
+  }
+  .head input{
+    margin: 0 1rem 0 0;
+    border-style: none;
+    padding: .6rem;
+    max-width: 100px;
+    border-radius: 5px;
+    
+  }
+  .col{
+    background-color: rgb(128, 42, 42);
+    height: 300px;
+    padding: .5rem;
+  }
+  .row{
+    padding: .5rem;
+    margin: .5rem;
+    color: aliceblue;
+    background-color: rgb(59, 17, 17);
+    max-width: 130px;
+    border-radius: 5px;
+  }
 </style>
