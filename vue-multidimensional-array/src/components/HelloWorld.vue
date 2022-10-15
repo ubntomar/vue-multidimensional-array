8eb1c40a3e5816d7f8eb2e1b343bebf9c872b02d
Omar Hernandez <omar.a.hernandez.d@gmail.com>
2022-10-14 20:20:54 -0500

    reduce line lenght interactively

--- vue-multidimensional-array/src/components/HelloWorld.vue
+++ vue-multidimensional-array/src/components/HelloWorld.vue
@@ -1,217 +1,305 @@
 <script setup lang="ts">
+import { ref, onMounted, computed, reactive } from "vue";
+import type { Ref } from "vue";
+defineProps<{ msg: string }>();
 
-import { ref , onMounted, computed, reactive} from 'vue'
-import type {Ref} from 'vue'
-defineProps<{ msg: string }>()
+const boxes: Ref<any> = ref([]);
+const containerHeadValues = ref({ buttonMsj: "Start", ipAddress: "" });
+const boxesId: Ref<any> = ref([]);
+boxesId.value.push(1);
 
-const boxes:Ref<any> =ref([])
-const containerHeadValues=ref({buttonMsj:'Start',ipAddress:""})
-const boxesId:Ref<any>=ref([])
-boxesId.value.push(1)
-
-
-const createFirstRow=()=>{
-  if(containerHeadValues.value.buttonMsj==='Start' && containerHeadValues.value.ipAddress !=="" ){
-      boxes.value.push(
-          {id: 1, descripcion: 'Leo', ip: containerHeadValues.value.ipAddress,isAllowedGrowingToDown:true,isAllowedGrowingToRight:false,rowPosition:0,colPosition:0,parent:0,childs:[],isthereChildToRight:false,isthereChildToDown:false,lineToRightLong:0,isToShow:true },
-        )
-      containerHeadValues.value.buttonMsj='Restart'
-    }else{
-      boxes.value=[]
-      containerHeadValues.value.ipAddress=""
-      containerHeadValues.value.buttonMsj='Start'
+const createFirstRow = () => {
+  if (
+    containerHeadValues.value.buttonMsj === "Start" &&
+    containerHeadValues.value.ipAddress !== ""
+  ) {
+    boxes.value.push({
+      id: 1,
+      descripcion: "Leo",
+      ip: containerHeadValues.value.ipAddress,
+      isAllowedGrowingToDown: true,
+      isAllowedGrowingToRight: false,
+      rowPosition: 0,
+      colPosition: 0,
+      parent: 0,
+      childs: [],
+      isthereChildToRight: false,
+      isthereChildToDown: false,
+      lineToRightLong: 0,
+      isToShow: true,
+    });
+    containerHeadValues.value.buttonMsj = "Restart";
+  } else {
+    boxes.value = [];
+    containerHeadValues.value.ipAddress = "";
+    containerHeadValues.value.buttonMsj = "Start";
   }
-}
+};
 
-const addChild=(fatherBox:any,direction:string)=>{
-  let fatherId:number=fatherBox.id
-  let newChildIdFetched:number=boxesId.value.at(-1)+1
-  boxesId.value.push(newChildIdFetched)
-  let newChildRowPosition:number
-  let newChildColPosition:number
-  const newChildId:number=newChildIdFetched
-  if(direction==='toBottom'){
-    fatherBox.childs.push({id:newChildIdFetched,direction:'toBottom'})
-    fatherBox.isAllowedGrowingToDown=false
-    fatherBox.isAllowedGrowingToRight=true
-    fatherBox.isthereChildToDown=true
-    newChildRowPosition=fatherBox.rowPosition+1
-    newChildColPosition=fatherBox.colPosition
-  }else{
-    fatherBox.childs.push({id:newChildIdFetched,direction:'toRight'})
-    fatherBox.isAllowedGrowingToDown=true
-    fatherBox.isAllowedGrowingToRight=false
-    fatherBox.isthereChildToRight=true
-    newChildRowPosition=fatherBox.rowPosition
-    newChildColPosition=fatherBox.colPosition+1
-    recalculateBoxesLocation(newChildId,newChildColPosition)
+const addChild = (fatherBox: any, direction: string) => {
+  let fatherId: number = fatherBox.id;
+  let newChildIdFetched: number = boxesId.value.at(-1) + 1;
+  boxesId.value.push(newChildIdFetched);
+  let newChildRowPosition: number;
+  let newChildColPosition: number;
+  const newChildId: number = newChildIdFetched;
+  if (direction === "toBottom") {
+    fatherBox.childs.push({ id: newChildIdFetched, direction: "toBottom" });
+    fatherBox.isAllowedGrowingToDown = false;
+    fatherBox.isthereChildToDown = true;
+    newChildRowPosition = fatherBox.rowPosition + 1;
+    newChildColPosition = fatherBox.colPosition;
+  } else {
+    fatherBox.childs.push({ id: newChildIdFetched, direction: "toRight" });
+    fatherBox.isAllowedGrowingToDown = true;
+    fatherBox.isAllowedGrowingToRight = false;
+    fatherBox.isthereChildToRight = true;
+    newChildRowPosition = fatherBox.rowPosition;
+    newChildColPosition = fatherBox.colPosition + 1;
+    recalculateBoxesLocation(newChildId, newChildColPosition);
   }
 
-  boxes.value.push(
-      {id: newChildId, descripcion: 'RB', ip: containerHeadValues.value.ipAddress,isAllowedGrowingToDown:true,isAllowedGrowingToRight:true,rowPosition:newChildRowPosition,colPosition:newChildColPosition,parent:fatherId,childs:[],isthereChildToRight:false,isthereChildToDown:false,lineToRightLong:0,isToShow:true },
-    )
-
-}
-const recalculateBoxesLocation=(newChildId:number,newOccupiedColPosition:number)=>{
-  boxes.value.forEach((box: { id: number,colPosition:number, }) => {
-    if(box.id!==newChildId && box.colPosition>=newOccupiedColPosition){
-      let colPositionresponse=findFatherColPosition(box.id)
-      if ( colPositionresponse.found && (colPositionresponse.value< newOccupiedColPosition) ) { 
-        findFatherChildToRightAndExtendLine(box.id)
+  boxes.value.push({
+    id: newChildId,
+    descripcion: "RB",
+    ip: containerHeadValues.value.ipAddress,
+    isAllowedGrowingToDown: true,
+    isAllowedGrowingToRight: true,
+    rowPosition: newChildRowPosition,
+    colPosition: newChildColPosition,
+    parent: fatherId,
+    childs: [],
+    isthereChildToRight: false,
+    isthereChildToDown: false,
+    lineToRightLong: 0,
+    isToShow: true,
+  });
+};
+const recalculateBoxesLocation = (newChildId: number, newOccupiedColPosition: number) => {
+  boxes.value.forEach((box: { id: number; colPosition: number }) => {
+    if (box.id !== newChildId && box.colPosition >= newOccupiedColPosition) {
+      let colPositionresponse = findFatherColPosition(box.id);
+      if (
+        colPositionresponse.found &&
+        colPositionresponse.value < newOccupiedColPosition
+      ) {
+        findFatherChildToRightAndExtendLine(box.id);
       }
-      box.colPosition+=1
+      box.colPosition += 1;
     }
   });
-}
+};
 
-let findFatherColPosition=(idChild:number):any=>{
-  let colPosition={
-    found:false,
-    value:0
-  }
-  boxes.value.forEach((box: { id: number,childs:any,lineToRightLong:number,isthereChildToRight:boolean, colPosition:number}) => {
-    if(box.childs.find((child:any)=>child.id===idChild)){
-      if(box.isthereChildToRight){
-        colPosition.found=true
-        colPosition.value=box.colPosition
-      }
-    }
-  })
-  return colPosition
-}
-const findFatherChildToRightAndExtendLine=(idChild:number)=>{
-  boxes.value.forEach((box: { id: number,childs:any,lineToRightLong:number,isthereChildToRight:boolean }) => {
-    if(box.childs.find((child:any)=>child.id===idChild)){
-      if(box.isthereChildToRight)
-      box.lineToRightLong===0?box.lineToRightLong+=12:box.lineToRightLong+=11
-    }
-  })
-}
-const findFatherChildAndDeleteLine=(idChild:number)=>{
-  boxes.value.forEach((box: { id: number,childs:any,lineToRightLong:number,isthereChildToRight:boolean,isthereChildToDown:boolean,isAllowedGrowingToRight:boolean,isAllowedGrowingToDown:boolean }) => {
-    let deleteChildRef:boolean=false
-    box.childs.forEach((childRef:any)=>{
-      if (childRef.id===idChild) {
-        if(childRef.direction==='toRight'){
-          box.lineToRightLong=0
-          box.isthereChildToRight=false
-          box.isAllowedGrowingToRight=true
-        } 
-        if(childRef.direction==='toBottom'){
-          box.isthereChildToDown=false
-          box.isAllowedGrowingToDown=true
+let findFatherColPosition = (idChild: number): any => {
+  let colPosition = {
+    found: false,
+    value: 0,
+  };
+  boxes.value.forEach(
+    (box: {
+      id: number;
+      childs: any;
+      lineToRightLong: number;
+      isthereChildToRight: boolean;
+      colPosition: number;
+    }) => {
+      if (box.childs.find((child: any) => child.id === idChild)) {
+        if (box.isthereChildToRight) {
+          colPosition.found = true;
+          colPosition.value = box.colPosition;
         }
-        deleteChildRef=true
       }
-    })
-    if(deleteChildRef)  box.childs=box.childs.filter((child:any)=>child.id!==idChild)
-  })
-}
-
-const hideBoxes=(box:any)=>{
-  findFatherChildAndDeleteLine(box.id)
-  box.isToShow=false
-  box.lineToRightLong=0
-  box.isthereChildToRight=false
-  box.isthereChildToDown=false
-  box.childs.forEach((childId:any) => {
-    boxes.value.find((recursiveBox:any)=>{
-      if(recursiveBox.id===childId.id){
-        recursiveBox.isToShow=false
-        hideBoxes(recursiveBox)
+    }
+  );
+  return colPosition;
+};
+const findFatherChildToRightAndExtendLine = (idChild: number) => {
+  boxes.value.forEach(
+    (box: {
+      id: number;
+      childs: any;
+      lineToRightLong: number;
+      isthereChildToRight: boolean;
+    }) => {
+      if (box.childs.find((child: any) => child.id === idChild)) {
+        if (box.isthereChildToRight)
+          box.lineToRightLong === 0
+            ? (box.lineToRightLong += 12)
+            : (box.lineToRightLong += 11);
       }
+    }
+  );
+};
+const findFatherChildAndDeleteLine = (idChild: number) => {
+  boxes.value.forEach(
+    (box: {
+      id: number;
+      childs: any;
+      lineToRightLong: number;
+      isthereChildToRight: boolean;
+      isthereChildToDown: boolean;
+      isAllowedGrowingToRight: boolean;
+      isAllowedGrowingToDown: boolean;
+    }) => {
+      let deleteChildRef: boolean = false;
+      box.childs.forEach((childRef: any) => {
+        if (childRef.id === idChild) {
+          if (childRef.direction === "toRight") {
+            box.lineToRightLong = 0;
+            box.isthereChildToRight = false;
+            box.isAllowedGrowingToRight = true;
+          }
+          if (childRef.direction === "toBottom") {
+            box.isthereChildToDown = false;
+            box.isAllowedGrowingToDown = true;
+          }
+          deleteChildRef = true;
+        }
+      });
+      if (deleteChildRef)
+        box.childs = box.childs.filter((child: any) => child.id !== idChild);
+    }
+  );
+};
 
-    })
-    
+const hideBoxes = (box: any) => {
+  findFatherChildAndDeleteLine(box.id);
+  box.isToShow = false;
+  box.lineToRightLong = 0;
+  box.isthereChildToRight = false;
+  box.isthereChildToDown = false;
+  box.childs.forEach((childId: any) => {
+    boxes.value.find((recursiveBox: any) => {
+      if (recursiveBox.id === childId.id) {
+        recursiveBox.isToShow = false;
+        hideBoxes(recursiveBox);
+      }
+    });
   });
-}
-
-
-
-const allocatePosition=computed(()=>{
-  return (box:any)=>{
-    let style='display:none;'
-    if(box.isToShow)
-      style=`grid-area: ${box.rowPosition+1} / ${box.colPosition+1} / ${box.rowPosition+2} / ${box.colPosition+2};`
-    return style
-  }
-})
-const showArrowtoRight=computed(()=>{
-  return (box:any)=>`${box.isthereChildToRight?`display:block;transform: translate(${box.lineToRightLong==0?'.5':box.lineToRightLong/2}rem,-50%) rotate(90deg);height: ${box.lineToRightLong==0?'1':box.lineToRightLong}rem;`:'display:none;'}`
-})
-const showArrowtoDown=computed(()=>{
-  return (box:any)=>`${box.isthereChildToDown?'display:block;':'display:none;'}`
-})
+};
+const moveSelectedToLeft =(clickedBox: any) => {
+  clickedBox.lineToRightLong-=11
+  clickedBox.childs.forEach((arrayInsideChildId: any) => {
+    boxes.value.find((box: any) => {
+      if (box.id === arrayInsideChildId.id) {
+        if (arrayInsideChildId.direction === "toRight") {
+          box.colPosition-=1
+          moveTargetedChildsToLeft(box);
+          console.log('end');
+          return;
+        }
+      }
+    });
+  });
+};
+const moveTargetedChildsToLeft = (ParentBoxesToLeft: any) => {
+  ParentBoxesToLeft.childs.forEach((arrayInsideChildId: any) => {
+    boxes.value.find((Box: any) => {
+      if (Box.id === arrayInsideChildId.id) {
+          Box.colPosition-=1
+          moveTargetedChildsToLeft(Box)
+        }
+    });
+  });
+  console.log('inside fn');
+  
+};
+const allocatePosition = computed(() => {
+  return (box: any) => {
+    let style = "display:none;";
+    if (box.isToShow)
+      style = `grid-area: ${box.rowPosition + 1} / ${box.colPosition + 1} / ${
+        box.rowPosition + 2
+      } / ${box.colPosition + 2};`;
+    return style;
+  };
+});
+const showArrowtoRight = computed(() => {
+  return (box: any) =>
+    `${
+      box.isthereChildToRight
+        ? `display:block;transform: translate(${
+            box.lineToRightLong == 0 ? ".5" : box.lineToRightLong / 2
+          }rem,-50%) rotate(90deg);height: ${
+            box.lineToRightLong == 0 ? "1" : box.lineToRightLong
+          }rem;`
+        : "display:none;"
+    }`;
+});
+const showArrowtoDown = computed(() => {
+  return (box: any) => `${box.isthereChildToDown ? "display:block;" : "display:none;"}`;
+});
 
 onMounted(() => {
-  boxes.value.shift()
-})
+  boxes.value.shift();
+});
 </script>
 
 <template>
   <div class="content">
-  <div class="head">
-    <input v-model="containerHeadValues.ipAddress" type="text" />
-    <button @click="createFirstRow()">{{ containerHeadValues.buttonMsj }}</button>
-  </div>
-  <main>
-    <div
-      :style="allocatePosition(box)"
-      class="box"
-      v-for="(box, index) in boxes" 
-      :key="index"
-    >
-      <img class="ellipse-up" src="../assets/Ellipse-up.svg" alt="" />
-      <img
-        :style="showArrowtoDown(box)"
-        class="ellipse-down"
-        src="../assets/Ellipse-right.svg"
-        alt=""
-      />
-      <div class="box-menu">
-        <img src="../assets/Vector-Mini-Menu.svg" alt="" />
-        <img @click="hideBoxes(box)" src="../assets/Vector-Close.svg" alt="" />
-      </div>
-      <div class="box-body">
-        <img
-          :style="showArrowtoRight(box)"
-          class="ellipse-right"
-          src="../assets/Ellipse-down.svg"
-          alt=""
-        />
-        <img
-          v-if="box.isAllowedGrowingToDown"
-          @click="addChild(box, 'toBottom')"
-          class="box-body--arrow"
-          src="../assets/Vector-Down.svg"
-          alt=""
-        />
-        <div class="box-input">
-          <img
-            class="box-input__success"
-            src="../assets/Vector-success.svg"
-            alt="successIP"
-          />
-          <input type="text" v-model="box.ip" />
-        </div>
-        <img
-          v-if="box.isAllowedGrowingToRight"
-          @click="addChild(box, 'toRight')"
-          class="box-body--arrow"
-          src="../assets/Vector-Right.svg"
-          alt=""
-        />
-      </div>
-      <div class="footer">
-        <p>id {{ box.id }} ({{ box.rowPosition }},{{ box.colPosition }})</p>
-      </div>
+    <div class="head">
+      <input v-model="containerHeadValues.ipAddress" type="text" />
+      <button @click="createFirstRow()">{{ containerHeadValues.buttonMsj }}</button>
     </div>
-  </main>
+    <main>
+      <div
+        :style="allocatePosition(box)"
+        class="box"
+        v-for="(box, index) in boxes"
+        :key="index"
+      >
+        <img class="ellipse-up" src="../assets/Ellipse-up.svg" alt="" />
+        <img
+          :style="showArrowtoDown(box)"
+          class="ellipse-down"
+          src="../assets/Ellipse-right.svg"
+          alt=""
+        />
+        <div class="box-menu">
+          <img src="../assets/Vector-Mini-Menu.svg" alt="" />
+          <img @click="hideBoxes(box)" src="../assets/Vector-Close.svg" alt="" />
+        </div>
+        <div class="box-body">
+          <img
+            :style="showArrowtoRight(box)"
+            @dblclick="moveSelectedToLeft(box)"
+            class="ellipse-right"
+            src="../assets/Ellipse-right.svg"
+            alt=""
+          />
+          <img
+            v-if="box.isAllowedGrowingToDown"
+            @click="addChild(box, 'toBottom')"
+            class="box-body--arrow"
+            src="../assets/Vector-Down.svg"
+            alt=""
+          />
+          <div class="box-input">
+            <img
+              class="box-input__success"
+              src="../assets/Vector-success.svg"
+              alt="successIP"
+            />
+            <input type="text" v-model="box.ip" />
+          </div>
+          <img
+            v-if="box.isAllowedGrowingToRight"
+            @click="addChild(box, 'toRight')"
+            class="box-body--arrow"
+            src="../assets/Vector-Right.svg"
+            alt=""
+          />
+        </div>
+        <div class="footer">
+          <p>id {{ box.id }} ({{ box.rowPosition }},{{ box.colPosition }})</p>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <style scoped lang="scss">
-.content{
+.content {
   width: max-content;
   min-width: 100vw;
   min-height: 100vh;
@@ -276,7 +364,7 @@ main {
       transform: translate(80%, -80%);
     }
     & input {
-      font-size: .8rem;
+      font-size: 0.8rem;
       color: rgba($color: #102027, $alpha: 0.8);
       width: 6.87rem;
       border: 1px solid rgba($color: #102027, $alpha: 0.2);
@@ -304,12 +392,16 @@ main {
 .ellipse-right {
   position: absolute;
   top: 50%;
-  right: 0;
-  width: 3px;
-  background-color: rgb(252, 252, 252);
+  right: -5px;
+  width: 11px;
+  background: rgba(34, 187, 40, 0.54);
+  z-index: 99;
+  &:hover{
+    background: rgba(229, 249, 80, 0.54);
+  }
 }
 .footer {
-  font-size: .75rem;
+  font-size: 0.75rem;
   padding: 3px 0;
   & p {
     width: min-content;
